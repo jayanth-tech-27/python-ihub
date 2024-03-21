1e26440051e2c94b61533e74f9c77034a0b245a3
jayanth <jayanthnaidu2710@gmail.com>
2024-03-21 14:32:01 +0530

    added add in main

---
+++ jayanth2.py
@@ -0,0 +1,14 @@
+import time
+x1=int(input("enter the value of x1:"))
+x2=int(input("enter the value of x2:"))
+res1=x1+x2
+print("the result set is :",res1)
+print()
+time.sleep(2)
+print("jayanth")
+
+
+x1=int(input("enter the value of x1"))
+x2=int(input("enter the value of x2"))
+res1=x1-x2
+print("the result set is ",res1)
