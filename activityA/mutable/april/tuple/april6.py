09dc77dd36e2988227ac873c55d14aa8a75cbbdb
jayanth <jayanthnaidu2710@gmail.com>
2024-04-17 19:37:16 +0530

    added add in main

---
+++ activityA/mutable/april/tuple/april6.py
@@ -0,0 +1,12 @@
+import time
+T1=eval(input("enter the tuple_data:"))
+print(T1)
+print()
+print(type(T1))
+print()
+print("==fetcting the data from tuple==")
+for a1 in T1:
+    print(a1)
+print()
+time.sleep(2)
+print("end of code")
---
+++ activityA/mutable/april/tuple/april7.py
@@ -0,0 +1,10 @@
+import time
+T1=eval(input("enter the data:"))
+print(T1)
+print()
+print(type(T1))
+print("==fetcting the data from tuple===")
+for a1 in T1:
+    print(a1)
+time.sleep(2)
+print("end of code")
