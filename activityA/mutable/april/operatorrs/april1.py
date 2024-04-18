9c6f60fbe2efad924cd7bd9f0d9bb9faae6f3a89
jayanth <jayanthnaidu2710@gmail.com>
2024-04-18 20:37:00 +0530

    added add in main

---
+++ activityA/mutable/april/operatorrs/april1.py
@@ -0,0 +1,21 @@
+import time
+x1=eval(input("enter the value of x1:"))
+x2=eval(input("enter the value of x2:"))
+print()
+print("===using the operators====")
+res1=x1+x2
+print(res1)
+res2=x1-x2
+print(res2)
+res3=x1*x2
+print(res3)
+res4=x1/x2
+print(res4)
+res5=x1//x2
+print(res5)
+res6=x1%x2
+print(res6)
+res7=x1**x2
+print(res7)
+time.sleep(2)
+print("end of code")
---
+++ activityA/mutable/april/operatorrs/april2.py
@@ -0,0 +1,17 @@
+import time
+x1=eval(input("enter the value of x1:"))
+x2=eval(input("enter the value of x2:"))
+print()
+print("==assignment operators====")
+x1+=x2
+print("sum is :",x1)
+x1-=x2
+print("sub is :",x2)
+x1*=x2
+print(x1)
+x1//=x2
+print(x1)
+x1%=x2
+print(x1)
+time.sleep(2)
+print("end of code")
