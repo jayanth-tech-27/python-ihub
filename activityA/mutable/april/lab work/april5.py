8e6368a100d70087dad00a25230238c167a51547
jayanth <jayanthnaidu2710@gmail.com>
2024-04-29 10:58:55 +0530

    added add in main

---
+++ activityA/mutable/april/lab work/april5.py
@@ -0,0 +1,26 @@
+# def add(a,b):
+#     return a + b
+# result = add(3,5)
+# print(result)
+
+# def add(a,b):
+#     return a+b
+# result=add(2242,2323)
+# # # print(result)
+
+
+# def myfunction():
+#   return 3+3
+# print("Hello, World!")
+
+# print(myfunction())
+
+# def myword():
+#   return 9+9
+# print("hello,world")
+# print(myword())
+
+
+def myworld():
+    return "awsds"
+print(myworld())
